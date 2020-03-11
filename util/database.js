076c096281a3a4b1864935fc7b3ccd4d8e35334b
Wilburt Herrera <wilburt_h@hotmail.com>
2020-03-10 23:30:01 -0700

    database updated

--- util/database.js
+++ util/database.js
@@ -1,10 +1,10 @@
-// const mysql = require('mysql2');
+const mysql = require('mysql2');
 
-// const pool = mysql.createPool({
-//     host: 'remotemysql.com',
-//     user: '',
-//     database: '',
-//     password: ''
-// });
+const pool = mysql.createPool({
+    host: 'remotemysql.com',
+    user: '8yI7g8YPVw',
+    database: '8yI7g8YPVw',
+    password: 'I33R35tUCk'
+});
 
-// module.exports = pool.promise();
+module.exports = pool.promise();
